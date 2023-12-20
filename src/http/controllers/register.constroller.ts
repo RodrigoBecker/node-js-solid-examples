b3bbd098ffb5e314a5f20fed9cf9092c3cdc77e9
Becker <rodrigo.beckermore@gmail.com>
2023-12-20 00:49:36 -0300

    chore: add fetures and segregation code

---
+++ src/http/controllers/register.constroller.ts
@@ -0,0 +1,30 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { prisma } from '@/lib/prisma'
+import { z } from 'zod'
+
+import { hash } from 'bcryptjs'
+import { registerUseCase } from '@/use-cases/register'
+
+
+export async function register(request: FastifyRequest, replay: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    await registerUseCase({
+      name,
+      email,
+      password
+    })
+
+  } catch (err) {
+    return replay.status(409).send()
+  }
+
+  return replay.status(201).send()
+}
